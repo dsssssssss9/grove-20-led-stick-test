714e6477e78bc464496729bd9980e7301c2d6c6d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-09-02 14:34:13 +0100

    reverse direction by putting -1 in rotate block

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ let strip = neopixel.create(DigitalPin.P2, 20, NeoPixelMode.RGB)
 strip.setBrightness(40)
 strip.showRainbow(1, 360)
 basic.forever(function () {
-    strip.rotate(1)
+    strip.rotate(-1)
     strip.show()
 })
